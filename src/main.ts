9af9d53808b57b95d84126d2f1b6d3899d164cad
Aaqibhafeezkhan <aaqibhafeezkhan@gmail.com>
2026-04-15 22:24:15 +0530

    fix: explicit vue module initialization to solve object prototype error

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import Vue from 'vue';
+import VueModule from 'vue';
 import singleSpaVueModule from 'single-spa-vue';
 import MediaApp from './MediaApp.vue';
 
+const Vue = (VueModule as any).default || VueModule;
 const singleSpaVue = (singleSpaVueModule as any).default || singleSpaVueModule;
 
 const vueLifecycles = singleSpaVue({
