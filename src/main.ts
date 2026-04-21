fa69d9f9748ebe97f242fba624549f451e8fc16a
Aaqibhafeezkhan <aaqibhafeezkhan@gmail.com>
2026-04-21 06:18:40 +0530

    fixes for responsive design

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import VueModule from 'vue';
-import singleSpaVueModule from 'single-spa-vue';
+import Vue from 'vue';
+import singleSpaVue from 'single-spa-vue';
 import MediaApp from './MediaApp.vue';
-
-const Vue = (VueModule as any).default || VueModule;
-const singleSpaVue = (singleSpaVueModule as any).default || singleSpaVueModule;
 
 const vueLifecycles = singleSpaVue({
   Vue,
   appOptions: {
-    render(h: any) {
-      return h(MediaApp);
-    },
+    render: (h: any) => h(MediaApp),
   },
 });
 
